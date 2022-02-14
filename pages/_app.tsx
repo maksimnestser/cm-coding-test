37c53f2357d4401668b1bad62f04cada6b878294
Maksim Nestser <maksim.nestser@leverx.com>
2022-02-14 14:14:37 +0300

    create theme and init QueryClient

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const theme = createTheme({
+  typography: {
+    fontFamily: "Poppins, sans-serif",
+  },
+  palette: {
+    text: {
+      primary: "#4b4b4b",
+      secondary: "#555555",
+    },
+    primary: {
+      main: "#2a7bab",
+      dark: "#01619b",
+    },
+  },
+});
+
+export const queryClient = new QueryClient();
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />;
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default App;
